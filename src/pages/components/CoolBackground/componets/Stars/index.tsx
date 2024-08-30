436b4f1d69eddf6d13038bc553f585d8c6e8d9f4
lichao <lichao@yilan.tv>
2024-08-30 20:08:26 +0800

    feat: icon

--- src/pages/components/CoolBackground/componets/Stars/index.tsx
+++ src/pages/components/CoolBackground/componets/Stars/index.tsx
@@ -2,14 +2,18 @@ import React, { useState } from "react";
 import { randomInSphere } from "../../../../../utils";
 import RotatingPoints from "../RotatingPoints";
 
-const Stars: React.FC<JSX.IntrinsicElements["group"]> = (props) => {
+type StarsProps = JSX.IntrinsicElements["group"] & {
+  color: string;
+};
+
+const Stars: React.FC<StarsProps> = ({ color, ...props }) => {
   const [sphere] = useState<Float32Array>(() =>
     randomInSphere(new Float32Array(5000), { radius: 1.5 })
   );
 
   return (
     <group rotation={[0, 0, Math.PI / 4]} {...props}>
-      <RotatingPoints positions={sphere} color="#007acc" size={0.005} />
+      <RotatingPoints positions={sphere} color={color} size={0.005} />
     </group>
   );
 };
--- src/pages/components/CoolBackground/index.tsx
+++ src/pages/components/CoolBackground/index.tsx
@@ -1,16 +1,40 @@
-import React, { Suspense } from "react";
+import React, { useState, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
+import { useLocation } from "react-router-dom"; // 导入 useLocation 钩子
+import { Spin, ColorPicker } from "antd";
 import "./index.less";
 import Stars from "./componets/Stars";
-import { Spin } from "antd";
+import { Color } from "antd/es/color-picker";
 
 const CoolBackground: React.FC = () => {
+  const [color, setColor] = useState<string>("#ffa0e0"); // 状态管理颜色
+
+  // 使用 useLocation 获取当前 URL 的查询参数
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const isDebugMode = searchParams.get("debug") === "true";
+
+  // 处理颜色变化
+  const handleColorChange = (newColor: Color) => {
+    setColor(newColor.toHexString());
+  };
+
   return (
-    <Suspense fallback={<Spin />}>
-      <Canvas camera={{ position: [0, 0, 1] }} className="background-canvas">
-        <Stars />
-      </Canvas>
-    </Suspense>
+    <>
+      {/* 仅当 debug 为 true 时显示 ColorPicker */}
+      {isDebugMode && (
+        <ColorPicker
+          value={color}
+          onChange={handleColorChange}
+          style={{ position: "absolute", top: 20, left: 20, zIndex: 1 }}
+        />
+      )}
+      <Suspense fallback={<Spin />}>
+        <Canvas camera={{ position: [0, 0, 1] }} className="background-canvas">
+          <Stars color={color} /> {/* 将颜色传递给 Stars 组件 */}
+        </Canvas>
+      </Suspense>
+    </>
   );
 };
 
